5bb9dd4d53102df4f4f63d3950e3402cf08949f1
NYDino <rfapp00@gmail.com>
2019-09-17 11:58:51 -0300

    adding login to the browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,8 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
     path('', include('snippets.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
